cf287212180cd10877e84e203fbf81264b1ecdce
Анастасия Пучнина <puchnina.nastya@gmail.com>
2018-11-16 22:30:17 +0300

    Static my_server added

--- source/server/server.c
+++ source/server/server.c
@@ -1,2 +1,13 @@
 #include "server.h"
 
+static struct server my_server;
+
+int server_initialize() 
+{
+    
+}
+
+int server_run() 
+{
+
+}
--- source/server/server.h
+++ source/server/server.h
@@ -1,11 +1,13 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include "my_socket.h"
+
 int server_initialize();
 int server_run();
 
 struct server {
-	int socket_fd_max;
+	int server_socket_fd;
 };
 
 #endif // SERVER_H
