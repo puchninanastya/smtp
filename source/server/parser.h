547182c6523ac9694451112c8cc410838b50c88c
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-01-14 12:55:19 +0300

    PCRE2 header with 8 code units width, Header for reg expressions

--- source/server/parser.h
+++ source/server/parser.h
@@ -1,6 +1,7 @@
 #ifndef SMTP_MTA_PARSER_H
 #define SMTP_MTA_PARSER_H
 
-
+#define PCRE2_CODE_UNIT_WIDTH 8 // to make use of the generic function names for code units widht (uint8_t, etc.)
+#include "pcre2.h"
 
 #endif //SMTP_MTA_PARSER_H
---
+++ source/server/rexprs.h
@@ -0,0 +1,6 @@
+#ifndef SMTP_MTA_REXPRS_H
+#define SMTP_MTA_REXPRS_H
+
+#define REG_CRLF "\\r\\n"
+
+#endif //SMTP_MTA_REXPRS_H
