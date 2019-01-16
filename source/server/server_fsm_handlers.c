368f7311fbf0eb959d4069e573e87b06ea9681c3
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-01-17 00:37:38 +0300

    Comments in handle_accepted

--- source/server/server_fsm_handlers.c
+++ source/server/server_fsm_handlers.c
@@ -29,7 +29,11 @@ int send_message_to_client( int client_fd, const char* response )
 int HANDLE_ACCEPTED( int client_fd, te_smtp_server_state nextState )
 {
     printf( "Handle accepted.\n" );
+
+    // set client socket as nonblocking
     fcntl( client_fd, F_SETFL, O_NONBLOCK );
+
+    // realloc array of clients
     if ( my_server.max_fd >= my_server.clients_size ) {
         printf( "Reallocing clients array.\n" );
         my_server.clients = realloc( my_server.clients,
@@ -37,13 +41,17 @@ int HANDLE_ACCEPTED( int client_fd, te_smtp_server_state nextState )
                 * sizeof( client_info* ) );
         my_server.clients_size += CLIENTS_REALLOC_STEP;
     }
+
+    // initialize client_info struct for this fd and add it to clients[]
     client_info* client = malloc( sizeof( client_info ) );
     memset( client, 0, sizeof( client_info ) );
     client->buffer = malloc( BUFFER_SIZE );
     memset( client->buffer, 0, BUFFER_SIZE );
     client->buffer_data_size = 0;
     client->smtp_state = SMTP_SERVER_ST_READY;
+
     my_server.clients[ client_fd ] = client;
+
     printf( "New client current smtp state: %d\n", my_server.clients[ client_fd ]->smtp_state );
     return nextState;
 }
