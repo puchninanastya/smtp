64d79f28f2fee3d4c0fe8a586fdbc074e0cd38fe
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-01-17 15:31:14 +0300

    Update response codes with official text

--- source/server/rexprs.h
+++ source/server/rexprs.h
@@ -28,15 +28,16 @@
 
 /* RESPONSES: */
 
-#define RE_RESP_READY "220 Ready\r\n"
-#define RE_RESP_CLOSE "221 Close\r\n"
-#define RE_RESP_OK "250 OK\r\n"
-#define RE_RESP_START_MAIL "354 Waiting for data\r\n"
+#define RE_RESP_READY "220 Service ready\r\n"
+#define RE_RESP_CLOSE "221 Service closing transmission channel\r\n"
+#define RE_RESP_OK "250 Requested mail action okay, completed\r\n"
+#define RE_RESP_START_MAIL "354 Start mail input; end with <CRLF>.<CRLF>\r\n"
 
-#define RE_RESP_ERR_WRONG_CMND "500 Syntax error, incorrect command\r\n"
-#define RE_RESP_ERR_NOT_IMPL_CMND "502 Command not implemeted\r\n"
+#define RE_RESP_ERR_WRONG_CMND "500 Syntax error, command unrecognised\r\n"
+#define RE_RESP_ERR_NOT_IMPL_CMND "502 Command not implemented\r\n"
 #define RE_RESP_ERR_BAD_SEQ "503 Bad sequence of commands\r\n"
-#define RE_RESP_ERR_MAILBOX_UNAVAIL "550 Mailbox unavailable\r\n"
-#define RE_RESP_ERR_MAILBOX_INCORRECT "553 Syntax error, incorrect email address\r\n"
+#define RE_RESP_ERR_MAILBOX_UNAVAIL "550 Requested action not taken: mailbox unavailable\r\n"
+#define RE_RESP_ERR_MEM_ALLOC "552 Requested mail action aborted: exceeded storage allocation\r\n"
+#define RE_RESP_ERR_MAILBOX_INCORRECT "553 Requested action not taken: mailbox name not allowed\r\n"
 
 #endif //SMTP_MTA_REXPRS_H
